a19b1a1f11434b06935c99ed79c529b9b6a46436
AndresCR24 <c-andres-98@hotmail.com>
2025-09-15 13:42:06 -0500

    se guardan pequeños cambios

--- src/main/java/com/indukitchen/indukitchen/persistence/crud/CrudFacturaEntity.java
+++ src/main/java/com/indukitchen/indukitchen/persistence/crud/CrudFacturaEntity.java
@@ -3,8 +3,6 @@ package com.indukitchen.indukitchen.persistence.crud;
 import com.indukitchen.indukitchen.persistence.entity.FacturaEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
 public interface CrudFacturaEntity extends JpaRepository<FacturaEntity, Long> {
@@ -13,7 +11,7 @@ public interface CrudFacturaEntity extends JpaRepository<FacturaEntity, Long> {
     @EntityGraph(attributePaths = {
             "carritoFactura",
             "carritoFactura.cliente",
-            "carritoFactura.productos"  // <- MUY IMPORTANTE
+            "carritoFactura.productos"
     })
     Optional<FacturaEntity> findById(Long id);
 }
